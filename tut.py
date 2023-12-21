cd2b989aab399d2ee543d73c1ddfe38f3f578b49
0m0kenny <kehindeoyesola@outlook.com>
2023-12-21 13:26:17 +0000

    Boolean Values

--- tut.py
+++ tut.py
@@ -5,55 +5,46 @@ Python Syntax to output data
 print("Hello World")
 
 #python Indentation
-'''
-sometimes python requires indentation eg if statement
-'''
+'''sometimes python requires indentation 
+eg if statement'''
 if 5 > 2:
     print("YES")
 
 #Python Variables 
-"""
-assign a valriable using variablename = 'variable' 
-variables can be different data types - string, number etc
-"""
+"""assign a valriable 
+using variablename = 'variable' variables can be different data types - string, number etc"""
 carname ='volvo'
 x = 50
 
-'''
-can display the sum of 2 variables
-'''
+'''can display the sum of 2 variables'''
 x = 5
 y =10
 (print(x+y))
 
-'''
-can create a new variable using data from 2 variables 
-'''
+'''can create a new variable using data from 2 variables '''
 x = 5
 y = 10
 z = x + y
 print(z)
 
-'''
-can assign values to multiple vairables in one line'''
+'''can assign values to multiple vairables in one line'''
 x, y, z = "orange", 'banana', 'cherry'
 print(x,y,z)
  
-'''
-can assign the same value to multiple variables'''
+'''can assign the same value to multiple variables'''
 x = y = z = 'orange'
 print (x, y, z)
 
-'''
-define  a global variable - created outside a function and can be used in and out of a function'''
+'''define  a global variable - 
+created outside a function and can be used in and out of a function'''
 x = 'awesome' #global variable
 def myfunc():
     print('python is ' + x)
 
 myfunc() 
 
-'''
-local variable is created inside function and can only be used inside function even  if global function with same key present'''
+'''local variable is created inside function 
+and can only be used inside function even  if global function with same key present'''
 x = 'awesome' #global variable
 def myfunc2():
     x ='fantastic' #local variable
@@ -62,8 +53,8 @@ def myfunc2():
 myfunc2()
 print('python is ' + x) #the x here would be global (awesome) since print syntax is outside the function
 
-'''
-can make a local variable global'''
+'''can make a local variable global
+using global function'''
 def myfunc3():
     global x
     x = 'amazing'
@@ -72,8 +63,8 @@ myfunc3()
 print('pyhon is ' + x) #the x here would be local turned global (amazing) even though print syntax outside function
 
 #Data Types
-'''
-to find the data type of your variable'''
+'''to find the data type of your variable
+use type()function'''
 x = 2  #an integer
 print(type(x)) 
 
@@ -96,14 +87,12 @@ x = True #a boolean
 print(type(x))
 
 #Python Numbers
-'''
-3 python number types'''
+'''3 python number types'''
 x = 1    # integer (whole number +/-)
 y = 2.8  # float (decimals, or 2e4 +/-)
 z = 1j   # complex (number+letter (j- imaginary part -check google))
 
-'''
-can convert numbers from one type to another except complex 
+'''can convert numbers from one type to another except complex 
 - cannot convert complex to a different type
 '''
 x = 1   
@@ -120,8 +109,8 @@ print(random.randrange(1, 10)) #random number displayed
 
 
 #Python Casting
-'''
-to specify variable type - int(), float() and string()'''
+'''to specify variable type 
+- use int(), float() and string()'''
 x = float(1)   # x will be float 1.0 although denoted as an integer
 y = str(2.8) # y will be string '2' although denoted as a float
 z = int("3") #z will be interger 3 although denoted as a string with ""
@@ -185,17 +174,39 @@ name = 'kenny'
 txt = 'my name is {} and i am {} years old'
 print(txt.format(name, age)) #write in order of variable that should be filled
 country = 'England'
-txt = 'my name is {1} and i am {0} years old and i live in {2}'
+txt = 'my name is {1} and i am {0} years old and i live in {2}' #or can use index of txt.format(variable0, variable1 etc) to specify the order
 print(txt.format(age, name, country))
 
+'''To insert illegal charcaters into string
+eg quotation marks(''), backslash(\), tab(t), new line(n) use the escape character -backslash (\) before the character'''
+txt = 'i am \'kenny\' and i am 3' # \ before each quote so that kenny can be 'kenny'
 
 
+#Boolean
+'''to evalutae any value and give true/false 
+use bool() function'''
+print(bool('hello')) #most values will be true if it has some contents -string, number, list,dict,tuple etc 
+print(bool(0)) #value None, number zero, empty list,dict,tuple,string etc are false
 
+'''an object made from a class with a _len_ function that returns 0 or False is False'''
+class myclass(): 
+    def _len_(self):
+        return 0
+myobj = myclass()
+print(bool(myobj))
 
+'''can execute code based on boolean answer of a function'''
+def myfunction():
+    return True
+if myfunction(): #if myfunction() returns true, print yes
+    print('yes')
+else:
+    print('no')
 
-
-
-
+''' some built in functions can return boolean values 
+eg isinstance() function - this determines if an object is of a certain data type'''
+x = 200
+print(isinstance(x,int)) #checks if x is an interger and returns true or false
 
 
 
